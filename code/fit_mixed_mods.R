35409c3cdee425ec136f3b956c5a72a2f20ced83
Dan Flynn <danfbflynn@gmail.com>
2016-02-12 16:55:49 -0500

    updates during Feb 12 meeting

--- code/fit_mixed_mods.R
+++ code/fit_mixed_mods.R
@@ -6,7 +6,7 @@ library(nlme)
 library(lme4) # for lmer
 library(sjPlot) # for plotting random effects of mixed effects models. install.packages('sjPlot',dep=T)
 
-datpath = "~/Dropbox/CoDyn/R files/10_08_2015_v6/CoDyn_heterogeneity" # or we can do URL method per Matt
+datpath = "~/Dropbox/CoDyn/R files/11_06_2015_v7/" # or we can do URL method per Matt
 
 # -- read in data
 dat <- read.csv(file.path(datpath, 'spatial_temporal_heterogeneity_diversity.csv'), row.names = 1)
@@ -15,23 +15,33 @@ dat <- dat[!is.na(dat$temporal_distance),] # 40 rows of NA
 
 # Goals:
 
-# 1. Temporal distance by dispersion. Temporal distance = bray-curtis across years within an experiment. Dispersion: bray-curtis across spatial replicates within an experiment. Take into account evenness J and other covariates
+# 1. Temporal distance by dispersion. Temporal distance = bray-curtis across adjacent years within an experiment. Dispersion: bray-curtis across spatial replicates within an experiment. Take into account evenness J and other covariates
 # - by biome (aquatic/terrestrial)
 # - by ecosystem type (herbaceous/woody/lake/river)
 
 # Questions: 1. Spatial vs temporal variability as predictor or response?
+#               - Spatial as driver.       
 #            2. Include other metrics from codyn package? variance_ratio, synchrony...
+#               - Maybe later. First things first. synchrony could be tried
 
 
 # Lmer models with hierarchical structure for the random effects
 
+
+# Variables to include
+
+#   
+
 m1 <- lmer(temporal_distance ~ dispersion + J + 
-           (1 | site_code / project_name.x / community_type),
+             + dispersion:J +
+           (dispersion | site_code / project_name / community_type),
            data = dat)
 
 summary(m1)
 ranef(m1) # Estimates for the random effects 
 fixef(m1) # Estimate (slopes) for the fixed effects of dispersion and evenness J. Both are positive
+sjp.lmer(m1, type = "fe")
+
 
 # Plotting the random effects. Since this model is intercept only, we can interpret these easily as the differences betwen community types or project names or sites in mean temporal distance.
 pdf("Random Effects plots m1.pdf", width = 10, height = 10)
@@ -40,12 +50,12 @@ dev.off(); system("open 'Random Effects plots m1.pdf' -a /Applications/Preview.a
 
 ## Now adding in a million covariates
 m2 <- lmer(temporal_distance ~ dispersion + J + 
-             plot_size..m2. + 
-            # X..plots + 
-             spatial_extent..m2. +
+             plot_size + 
+            
+             spatial_extent +
              dataset_length + 
-             time_step.y +
-             (1 | site_code / project_name.x / community_type),
+             time_step +
+             (1 | site_code / project_name / community_type),
            data = dat)
 summary(m2)
 ranef(m2) # Estimates for the random effects 
@@ -57,21 +67,21 @@ dev.off(); system("open 'Random Effects plots m2.pdf' -a /Applications/Preview.a
 
 m3 <- lmer(temporal_distance ~ dispersion + 
              J + 
-             plot_size..m2. + 
-             # X..plots + 
-             spatial_extent..m2. +
+             plot_size + 
+             num_plots + 
+             spatial_extent +
              dataset_length + 
-             time_step.y +
+             time_step +
              temp_C + 
              MAP_mm +
              S + 
-             lifespan..subannual..annual.or.longer. + 
+             lifespan + 
              succession + 
-             trophic_level..consumer..primary. + 
+             trophic_level + 
             # system + 
              broad_ecosystem_type + 
             # ANPP..g.m2. +
-             (1 | site_code / project_name.x / community_type),
+             (1 | site_code / project_name / community_type),
            data = dat)
 summary(m3)
 ranef(m3) # Estimates for the random effects 
@@ -86,17 +96,91 @@ dev.off(); system("open 'Random and Fixed Effects plots m3.pdf' -a /Applications
 
 
 
-m22 <- lmer(temporal_distance ~ dispersion + J + 
-             plot_size..m2. + 
-             # X..plots + 
-             spatial_extent..m2. +
+m22 <- lmer(temporal_distance ~ dispersion + #J + 
+              S +
+              ANPP + 
+             plot_size + 
+             num_plots + 
+             spatial_extent +
              dataset_length + 
-             time_step.y +
-             (dispersion | site_code / project_name.x / community_type),
+             time_step + 
+             (dispersion | site_code / project_name / community_type),
            data = dat)
 summary(m22)
 ranef(m22) # Estimates for the random effects 
 fixef(m22) # Estimate (slopes) for
+sjp.lmer(m22, type = "fe")
+
+
+
+
+m23 <- lmer(temporal_distance ~ dispersion + #J +  #dispersion:J + 
+            #  broad_ecosystem_type * taxa  +
+              succession +
+              trophic_level +
+              system +
+              #system:dispersion +
+              (dispersion | site_code / project_name / community_type),
+            data = dat)
+summary(m23)
+ranef(m23) # Estimates for the random effects 
+fixef(m23) # Estimate (slopes) 
+sjp.lmer(m23, type = "fe")
+
+
+##### Organism-level predictors
+
+
+m24 <- lmer(temporal_distance ~  dispersion + 
+              taxa  +
+              lifespan +
+              #trophic_level +
+              #system +
+              #system:dispersion +
+              (dispersion | site_code / project_name / community_type),
+            data = dat)
+summary(m24)
+ranef(m23) # Estimates for the random effects 
+fixef(m23) # Estimate (slopes) 
+sjp.lmer(m24, type = "fe")
+
+
+# For just longer-lifespan organisms, is there still going to be a relationship?
+m25 <- lmer(temporal_distance ~  dispersion + 
+              
+              (dispersion | site_code / project_name / community_type),
+            data = dat[dat$lifespan=="longer",])
+summary(m25)
+sjp.lmer(m25, type = "fe")
+
+
+
+### 
+
+
+d <- tapply(dat$dispersion,
+               dat$site_project_comm,
+               mean, na.rm=T)
+dt <- tapply(dat$temporal_distance,
+             dat$site_project_comm,
+             mean, na.rm=T)
+
+summary(lm(dt ~ d))
+plot(d, dt)
+abline(lm(dt ~ d))
+# Additional analyses:
+
+# Rarefactions in space and time to show that our data do show real relationship, not underestimating time or space component
+
+# Why is J more important than S?
+# J and S both calculated across all plots within that time step, within that study
+
+# BC sensitive to abundance. So instead of BC, look at turnover (as a metric not as sensitive to abundance)
+
+# Could add into model: ratio of spatial extent by dataset length
+
+
+# two different studies with either giant plots or giant spatial extent.
 
 
 
@@ -105,12 +189,26 @@ fixef(m22) # Estimate (slopes) for
 #   plot size       - plot_size..m
 #   total area      - spatial_extent..m2.
 #   total time span - dataset_length
-dat.agg <- aggregate(dat[c("temporal_distance","dispersion","spatial_extent..m2.","dataset_length")], 
-                     dat[c("site_code","project_name.x")],
+dat.agg <- aggregate(dat[c("temporal_distance","dispersion","spatial_extent","dataset_length","J")], 
+                     dat[c("site_code","project_name","community_type")],
                      mean, na.rm=T)
 
 colz = as.numeric(dat.agg$site_code)
 ccolz = rainbow(n = length(unique(colz)), v = 0.5, alpha = 0.7)[colz]
+
+plot(temporal_distance ~ dispersion, data = dat,
+     pch = 16, col = alpha("black", alpha = 0.01)) 
+
+with(dat[dat$system=="aquatic",], 
+     points(dispersion, temporal_distance, col = alpha('red', 0.5), pch = 16)
+     )
+with(dat[dat$system=="aquatic",], 
+  abline(lm(temporal_distance ~ dispersion), col = 'red') )
+
+
+with(dat[dat$system=="terrestrial",], points(dispersion, temporal_distance, col = alpha('blue', 0.5), pch = 16, ))
+with(dat[dat$system=="terrestrial",], 
+     abline(lm(temporal_distance ~ dispersion), col = 'blue') )
 
 
 plot(temporal_distance ~ dispersion, data = dat,
@@ -122,7 +220,50 @@ with(dat.agg,
 points(dispersion, temporal_distance,
         pch = 16, 
         col = ccolz,
-        cex = log(spatial_extent..m2.)/5))
+        cex = J*3))
+
+
+with(dat.agg,
+     points(dispersion, temporal_distance,
+            pch = 16, 
+            col = ccolz,
+            cex = dataset_length))
+
+
+######### Looking at relationships between our predictors
+plot(J ~ dispersion, data = dat) # yup they are related
+
+plot(S ~ dispersion, data = dat)
+
+
+plot(S ~ dispersion, data = dat, type = "n")
+with(dat[dat$system=="aquatic",], 
+     points(dispersion, S, col = alpha('red', 0.5), pch = 16)
+)
+with(dat[dat$system=="aquatic",], 
+     abline(lm(S ~ dispersion), col = 'red') )
+
+with(dat[dat$system=="terrestrial",], points(dispersion, S, col = alpha('blue', 0.5), pch = 16, ))
+with(dat[dat$system=="terrestrial",], 
+     abline(lm(S ~ dispersion), col = 'blue') )
+
+library(ggplot2)
+ggplot(dat, aes(dispersion, S, col = taxa)) + geom_point()
+
+
+ggplot(dat, aes(dispersion, S, col = taxa)) + geom_point() #+ geom_smooth()
+
+
+ggplot(dat, aes(dispersion, temporal_distance, col = taxa)) + geom_point() + geom_smooth(method="lm")
+
+
+ggplot(dat, aes(dispersion, temporal_distance, col = lifespan)) + geom_point() + geom_smooth(method="lm")
+
+
+# Ok, how about at different lifespans, does the effect of dataset length matter?
+
+ggplot(dat, aes(dispersion, temporal_distance, col = dataset_length)) + geom_point() + geom_smooth(method="lm") + facet_wrap(~lifespan)
+
 
 
 #levels(cut(log(dat.agg$spatial_extent..m2.)/5, 5))
